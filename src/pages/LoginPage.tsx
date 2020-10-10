cefd0e3ae472ef4164f671ef1aeb4da63e9ad290
dominikvrbic <dominikvrbic01@gmail.com>
2020-10-10 15:09:33 +0200

    init

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+interface Props {}
+
+export const LoginPage = (props: Props) => {
+  return <div></div>;
+};
---
+++ src/pages/Recipe.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+interface Props {}
+
+export const Recipe = (props: Props) => {
+  return <div></div>;
+};
