c42947ec8e64c54388bf52f229d756e522c8090f
dominikvrbic <dominikvrbic01@gmail.com>
2020-10-11 16:37:59 +0200

    footer desktop

--- src/assets.ts
+++ src/assets.ts
@@ -1,6 +1,8 @@
 import hat from './assets/images/hat.png';
 import { ReactComponent as Search } from './assets/images/search.svg';
 import SpinnerGif from './assets/images/spinner.gif';
+import GithubLogoBlue from './assets/images/github-logo-blue.svg';
+import InstagramLogo from './assets/images/instagram-logo.svg';
 
 export const apiUrl = 'http://localhost:1337';
-export { hat, Search, SpinnerGif };
+export { hat, Search, SpinnerGif, GithubLogoBlue, InstagramLogo };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { GithubLogoBlue, InstagramLogo } from '../assets';
 
 interface Props {}
 
 export const Footer = (props: Props) => {
-  return <div></div>;
+  return (
+    <div className='flex-shrink-0 flex  items-center bg-gray-100 py-6 md:py-2 h-24'>
+      <Link className='font-semibold px-5' to='/'>
+        home
+      </Link>
+      <div className='text-center w-full'>
+        designed & developed by Dominik Vrbić
+      </div>
+
+      <div className='flex ml-auto pr-5'>
+        <a
+          href='https://www.instagram.com/dominikvrbic/?hl=en'
+          target='_blank'
+          rel='noopener noreferrer'
+          style={{ marginRight: '40px' }}>
+          <img src={InstagramLogo} alt='Instagram Logo' />
+        </a>
+        <a
+          href='https://github.com/dominikvrbic'
+          target='_blank'
+          rel='noopener noreferrer'>
+          <img src={GithubLogoBlue} alt='Github Logo' />
+        </a>
+      </div>
+    </div>
+  );
 };
