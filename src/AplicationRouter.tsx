080b88613ff382393cd0478839126830971aaedf
dominikvrbic <dominikvrbic01@gmail.com>
2020-10-11 14:34:46 +0200

    moving footer and navbar changing route for recipe

--- src/AplicationRouter.tsx
+++ src/AplicationRouter.tsx
@@ -4,16 +4,20 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage';
 import { Profile } from './pages/Profile';
 import { Recipe } from './pages/Recipe';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 
 export const AplicationRouter = () => {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={LoginPage} />
         <Route exact path='/profile' component={Profile} />
-        <Route exact path='/recepe:slug' component={Recipe} />
+        <Route exact path='/recipe/:slug' component={Recipe} />
       </Switch>
+      <Footer />
     </Router>
   );
 };
