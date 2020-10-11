48f5c2fc8109a277934f8fd218243dc0e07cb89f
dominikvrbic <dominikvrbic01@gmail.com>
2020-10-11 12:53:01 +0200

    navbar style

---
+++ src/assets.ts
@@ -0,0 +1,4 @@
+import hat from './assets/images/hat.png';
+import { ReactComponent as Search } from './assets/images/search.svg';
+
+export { hat, Search };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { hat, Search } from '../assets';
 
 interface Props {}
 
 export const Navbar = (props: Props) => {
-  return <div></div>;
+  return (
+    <div className='flex flex-row justify-between items-center h-24 bg-gray-100 flex-shrink-0 py-6 md:py-2 '>
+      <img src={hat} alt='hat' className='cursor-pointer' />
+      <div className='pt-2 relative mx-right mx-4 text-gray-600'>
+        <input
+          className='border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none'
+          type='search'
+          name='search'
+          placeholder='Search'
+        />
+        <button type='submit' className='absolute right-0 top-0 mt-5 mr-4'>
+          <Search className='text-gray-600 h-4 w-4 fill-current' />
+        </button>
+      </div>
+    </div>
+  );
 };
