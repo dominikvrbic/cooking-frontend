b82298eb60cbb906fe7e77f9350c4886fed9a44b
dominikvrbic <dominikvrbic01@gmail.com>
2020-10-11 18:33:02 +0200

    Recipe page to use slug from url to search

--- src/pages/Recipe.tsx
+++ src/pages/Recipe.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { useFindRecipeQuery } from '../generated/graphql';
 import Spinner from '../components/Spinner';
-
+import { useParams } from 'react-router-dom';
+interface RouterParams {
+  slug: string;
+}
 interface Props {}
 
 export const Recipe = (props: Props) => {
+  const { slug } = useParams<RouterParams>();
   const { data, loading } = useFindRecipeQuery({
     variables: {
-      slug: 'test',
+      slug,
     },
   });
+  console.log(data);
   return <>{loading ? <Spinner /> : <div>{data?.recipes[0]?.name}</div>}</>;
 };
