120a04bfcd40bd6f339c9faf4da1630f49a9cfbc
dominikvrbic <dominikvrbic01@gmail.com>
2020-10-11 22:32:06 +0200

    footer and navbar fixes and adding mobx to project

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { GithubLogoBlue, InstagramLogo } from '../assets';
+import { useWindowSize } from 'react-use';
 
 interface Props {}
 
 export const Footer = (props: Props) => {
+  const { width } = useWindowSize();
+
   return (
-    <div className='flex-shrink-0 flex  items-center bg-gray-100 py-6 md:py-2 h-24'>
+    <div className='  w-full flex-shrink-0 flex  items-center bg-gray-100 py-6 md:py-2 h-24'>
       <Link className='font-semibold px-5' to='/'>
         home
       </Link>
-      <div className='text-center w-full'>
-        designed & developed by Dominik Vrbić
-      </div>
+      {width > 600 && (
+        <div className='text-center w-full'>
+          designed & developed by Dominik Vrbić
+        </div>
+      )}
 
       <div className='flex ml-auto pr-5'>
         <a
@@ -20,13 +25,13 @@ export const Footer = (props: Props) => {
           target='_blank'
           rel='noopener noreferrer'
           style={{ marginRight: '40px' }}>
-          <img src={InstagramLogo} alt='Instagram Logo' />
+          <img className='h-16 w-16' src={InstagramLogo} alt='Instagram Logo' />
         </a>
         <a
           href='https://github.com/dominikvrbic'
           target='_blank'
           rel='noopener noreferrer'>
-          <img src={GithubLogoBlue} alt='Github Logo' />
+          <img className='h-16 w-16' src={GithubLogoBlue} alt='Github Logo' />
         </a>
       </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import { hat, Search } from '../assets';
 import { Link } from 'react-router-dom';
-interface Props {}
+import { userStore } from '../userStore';
+import { observer } from 'mobx-react';
 
-export const Navbar = (props: Props) => {
+export const Navbar = observer(() => {
   return (
-    <div className='flex flex-row justify-between items-center h-24 bg-gray-100 flex-shrink-0 py-6 md:py-2 '>
+    <div className='flex flex-col  items-center h-42  bg-gray-100  pb-5 '>
       <Link to='/'>
         <img src={hat} alt='hat' className='cursor-pointer' />
       </Link>
       <ul className='flex'>
-        <Link to='/login'>
-          <li className='text-gray-600 mx-3 '>Login/Signup</li>
-        </Link>
-        {/* <Link to='/profile'>
-          <li className='text-gray-600 mx-3 '>Profile</li>
-        </Link> */}
+        {userStore.jwt ? (
+          <>
+            <Link to='/profile'>
+              <li className='text-gray-600 mx-3 '>Profile</li>
+            </Link>
+            <Link to='/profile'>
+              <li className='text-gray-600 mx-3 '>Sign out</li>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to='/login'>
+              <li className='text-gray-600 mx-3  '>Login</li>
+            </Link>
+            <Link to='/signup'>
+              <li className='text-gray-600 mx-3  '>Signup</li>
+            </Link>
+          </>
+        )}
       </ul>
-      <div className='pt-2 relative mx-right mx-4 text-gray-600 '>
+      <div className='pt-2 relative mx-right text-gray-600 '>
         <input
           className='border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none '
           type='search'
@@ -30,4 +44,4 @@ export const Navbar = (props: Props) => {
       </div>
     </div>
   );
-};
+});
