2c76ae61b8ddef0580f38efd67ffd330daeb5e7e
dominikvrbic <dominikvrbic01@gmail.com>
2020-10-12 19:56:19 +0200

    Persist user login state

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,9 +1,20 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const UserContext = createContext(null);
 
 export default ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    const userDataJSON = window.localStorage.getItem('user');
+    return userDataJSON ? JSON.parse(userDataJSON) : null;
+  });
+
+  useEffect(() => {
+    if (user) {
+      window.localStorage.setItem('user', JSON.stringify(user));
+    } else {
+      window.localStorage.clear();
+    }
+  }, [user]);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
